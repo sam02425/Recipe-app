73e4949e94360a69d6613b7aa6248dbaffe2e65e
Saumil Patel <saumilpatel56@yahoo.com>
2020-11-08 03:18:53 -0600

    manage user end point

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,7 +4,8 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
-class  Command(BaseCommand):
+
+class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
--- app/core/models.py
+++ app/core/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-
-class  UserManager(BaseUserManager):
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and save new user"""
@@ -25,7 +24,8 @@ class  UserManager(BaseUserManager):
 
         return user
 
-class  User(AbstractBaseUser, PermissionsMixin):
+
+class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
