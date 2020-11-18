0bb00a386861529f2b7e7d3d054baa622256f243
Saumil Patel <saumilpatel56@yahoo.com>
2020-11-18 05:23:28 -0600

    adding tag and ingredient

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag
-
+from core.models import Tag, Ingredient, Recipe
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag objects"""
@@ -10,3 +9,38 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name')
         read_only_Fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for an ingredient object"""
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
+        read_only_Fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'title', 'ingredients', 'tags', 'time_minutes', 'price',
+            'link'
+        )
+        read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
